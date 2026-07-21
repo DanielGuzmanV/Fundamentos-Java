4044e71fd5df8efb9603f88eaa101f953d102a42
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-07-21 09:42:06 -0400

    Refactoriazcion y mejora en la explicacion de null safety

--- src/conceptos_basicos/manejo_nulos/NullSafetyExplained.java
+++ src/conceptos_basicos/manejo_nulos/NullSafetyExplained.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 public class NullSafetyExplained {
 
-  // Validar nombre
+  // Forma 1: Metodo con programacion defensiva clasica
   public static void fnValidarNombre(String nombre) {
     if(nombre != null) {
       System.out.println("Longitud: " + nombre.length());
@@ -13,24 +13,28 @@ public class NullSafetyExplained {
     }
   }
 
-  public static void main(String[] args) {
-    System.out.println("=== Analizando seguridad ante nulos ===");
+  // Forma 2: El metodo retorna un Optional, un metodo que busca, procesa o devuelve algo que puede ser null
+  public static void fnValidarNombreOptional(String nombre) {
+    Optional<String> nombreOpcional = Optional.ofNullable(nombre);
 
-    String nombreNull = "Pamela"; // Aqui el puntero no apunta a nada
-
-    // Forma 1: Defensive programming
-    // ------------------------------
-    fnValidarNombre(nombreNull);
-
-    // Forma 2: optional<T> (forma moderna)
-    // ------------------------------------
-    Optional<String> nombreOpcional = Optional.ofNullable(nombreNull);
-
-    // Si la caja esta vacia, devuelve "Desconocido"
+    // 1. Si la caja esta vacia, devuelve "Desconocido"
     String resultado = nombreOpcional.orElse("Desconocido");
     System.out.println("Resultado seguro: " + resultado);
 
-    // Ejecutamos algo si hay algo de valor
-    nombreOpcional.ifPresent(n -> System.out.println("El nombre es: " +n));
+    // 2. Ejecutamos algo solo si hay un valor real dentro
+    nombreOpcional.ifPresent(n -> System.out.println("El nombre es: " + n));
+  }
+
+  public static void main(String[] args) {
+    System.out.println("=== Analizando seguridad ante nulos ===");
+
+    String nombreNull = null; // Aqui el puntero no apunta a nada
+    String nombreReal = "Alyx"; // Aqui el puntero apunta al nombre
+
+    System.out.println("--- Probando con NULL ---");
+    fnValidarNombre(nombreNull);
+
+    System.out.println("\n--- Probando con VALOR REAL ---");
+    fnValidarNombreOptional(nombreReal);
   }
 }
